99ceac922d6d828b2a3df3168968464a6a5895d6
Kayo Kikuchi <kkikuchi@c1r16s6.42tokyo.jp>
2021-10-30 17:36:20 +0900

    deal norm in env

--- src/bltin/env.c
+++ src/bltin/env.c
@@ -1,9 +1,9 @@
 #include "../../includes/minishell.h"
 
-static void show_env(void)
+static void	show_env(void)
 {
-	extern char **environ;
-	int i;
+	extern char	**environ;
+	int			i;
 
 	i = 0;
 	while (environ[i] != NULL)
@@ -14,7 +14,7 @@ static void show_env(void)
 	}
 }
 
-int minishell_env(char **argv)
+int	minishell_env(char **argv)
 {
 	if (argv[1] == NULL)
 		show_env();
